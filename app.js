569e950b533a68b2f4db86797d7d3753e8d83ce8
sandarmyint <1sandarmyint1@gmail.com>
2020-01-24 13:32:58 +0630

    renew

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `Min Ga Lar Par Sint!`
+      "text": `Min Ga Lar Par bya!`
     }
   }else if (received_message.text == "ni hao") {    
     // Create the payload for a basic text message, which
