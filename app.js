d8268cdf7445d5dad6fcb3d6dc1fc039e12455c8
sandarmyint <1sandarmyint1@gmail.com>
2020-02-07 01:38:05 +0630

    quick

--- app.js
+++ app.js
@@ -139,7 +139,8 @@ function handlePostback(sender_psid, received_postback) {
    let response;
   let payload = received_postback.payload;
   if (payload === 'STC') {
-    response = { "attachment":{
+    response = { "text": "Give your body measure!",
+      "attachment":{
       "type":"image", 
       "payload":{
         "url":"https://www.dummies.com/wp-content/uploads/how-to-get-your-body-measurements.jpg", 
