9e539a3fa4587d789895d10f2b3cb0c88b0f7289
sandarmyint <1sandarmyint1@gmail.com>
2020-02-11 01:31:16 +0630

    renew

--- app.js
+++ app.js
@@ -213,7 +213,35 @@ function handleMessage(sender_psid, received_message) {
     }
   }else if (received_message.text == "Yes!") {    
     response = {
-      "text": `Finally! your Inseam.`
+      "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+           {
+            "title":"Welcome!",
+            "image_url":"https://petersfancybrownhats.com/company_image.png",
+            "subtitle":"We have the right hat for everyone.",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://petersfancybrownhats.com/view?item=103",
+              "webview_height_ratio": "tall",
+            },
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://petersfancybrownhats.com",
+                "title":"View Website"
+              },{
+                "type":"postback",
+                "title":"Start Chatting",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD"
+              }              
+            ]      
+          }
+        ]
+      }
+    }
     }
   }
   callSendAPI(sender_psid, response);    
