10a075d2af1fe9216f807347190bd774103cbe40
sandarmyint <1sandarmyint1@gmail.com>
2020-03-04 00:50:28 +0630

    renew

--- app.js
+++ app.js
@@ -272,9 +272,6 @@ function handleMessage(sender_psid, received_message) {
     console.log('meta data',received_message);
     designAttachment == false;
     let attachment_url = received_message.attachments[0].payload.url;
-
-    
-
     response = {
       "attachment": {
         "type": "template",
