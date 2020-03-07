daab65fe1f8e93f414cda9bbc130785f1dd163cb
sandarmyint <1sandarmyint1@gmail.com>
2020-03-08 02:36:58 +0630

    renew

--- app.js
+++ app.js
@@ -158,18 +158,18 @@ function handleMessage(sender_psid, received_message) {
         "payload": {
           "template_type": "generic",
           "elements": [{
-            "title": "Wow this one will totally suits you.",
-            "subtitle": "I can't wait to sew it.",
+            "title": "Is this design?",
+            "subtitle": "Wow! this one will totally suits you.I can't wait to sew it.",
             "image_url": attachment_url,
             "buttons": [
               {
                 "type": "postback",
-                "title": "Yes!",
+                "title": "Yes, that one. :)",
                 "payload": "yes",
               },
               {
                 "type": "postback",
-                "title": "No!",
+                "title": "No, sorry. :(",
                 "payload": "no",
               }
             ],
@@ -222,12 +222,96 @@ function handlePostback(sender_psid, received_postback) {
     askforbeaded (sender_psid);
   }else if (payload === 'send_design') {
     let response1 = {"text" : "We will sew it look like the design you send."};
-    let response2 = {"text" : "well, send us the design you want to sew."};
+    let response2 = {"text":"For what kind of event?"};
+    let response3 = {"text":"These are the kinds of sewing we do in our shop."};
+    let response4 = {
+       "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+           {
+            "title":"A wedding?",
+            "image_url":"https://i.pinimg.com/236x/e6/77/cc/e677cc25d57a184fc8928a001f5f25c2--traditional-wedding-dresses-traditional-outfits.jpg",
+            "subtitle":"👰",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://i.pinimg.com/236x/e6/77/cc/e677cc25d57a184fc8928a001f5f25c2--traditional-wedding-dresses-traditional-outfits.jpg",
+              "webview_height_ratio": "tall",
+            },
+            "buttons":[
+             {
+                "type":"postback",
+                "title":"This one.",
+                "payload":"WEDDING"
+              }              
+            ]      
+          },
+          {
+            "title":"Occasion?",
+            "image_url":"https://i.pinimg.com/236x/a4/93/0d/a4930df067551676be9f50906b62ed56.jpg",
+            "subtitle":"💃",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://i.pinimg.com/236x/a4/93/0d/a4930df067551676be9f50906b62ed56.jpg",
+              "webview_height_ratio": "tall",
+            },
+            "buttons":[
+             {
+                "type":"postback",
+                "title":"This one.",
+                "payload":"OCCASION"
+              }              
+            ]      
+          },
+          {
+            "title":"Casual?",
+            "image_url":"https://i.pinimg.com/236x/8e/4f/34/8e4f3428ae5c12d2d91c7847ff087bfb--kebaya-indonesia-thai-dress.jpg",
+            "subtitle":"🤷",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://i.pinimg.com/236x/8e/4f/34/8e4f3428ae5c12d2d91c7847ff087bfb--kebaya-indonesia-thai-dress.jpg",
+              "webview_height_ratio": "tall",
+            },
+            "buttons":[
+             {
+                "type":"postback",
+                "title":"This one.",
+                "payload":"CASUAL"
+              }              
+            ]      
+          },
+          {
+            "title":"For a bechelor's degree?",
+            "image_url":"https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/29792720_1006980256115703_3053445385785054787_n.jpg?_nc_cat=108&_nc_sid=110474&_nc_ohc=MvKZ7Bf0e1oAX-_9g54&_nc_ht=scontent-sea1-1.xx&oh=d6203387d93b6ad874ddf661dab28425&oe=5E8106ED",
+            "subtitle":"👩‍🎓",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/29792720_1006980256115703_3053445385785054787_n.jpg?_nc_cat=108&_nc_sid=110474&_nc_ohc=MvKZ7Bf0e1oAX-_9g54&_nc_ht=scontent-sea1-1.xx&oh=d6203387d93b6ad874ddf661dab28425&oe=5E8106ED",
+              "webview_height_ratio": "tall",
+            },
+            "buttons":[
+             {
+                "type":"postback",
+                "title":"This one.",
+                "payload":"ABD"
+              }              
+            ]      
+          }
+        ]
+      }
+    }
+  };
+    //let response2 = {"text" : "well, send us the design you want to sew."};
     callSend(sender_psid, response1).then(()=>{
-      return callSend(sender_psid, response2);
+      return callSend(sender_psid, response2).then(()=>{
+        return callSend(sender_psid, response3).then(()=>{
+          return callSend(sender_psid, response4);
+        });
+      });
     });
-    designAttachment = true;
-      bdesignAttachment = false;
+   // designAttachment = true;
+   //   bdesignAttachment = false;
   }
   callSendAPI(sender_psid, response);
 }
