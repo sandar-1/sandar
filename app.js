f1f4963d6da9d0be420739c98d3cc22dc73841b3
sandarmyint <1sandarmyint1@gmail.com>
2020-04-23 16:56:14 +0630

    renew

--- app.js
+++ app.js
@@ -455,6 +455,7 @@ function handlePostback(sender_psid, received_postback) {
     response = {"text" : "Ok!"}
   }else if (payload === 'yes_sp') {
     saveData (sender_psid);
+    response = {"text" : "Okkk!"}
   }else if (payload === 'yes_right_measurment') {
     let response1 = {"text" : "which type of htamein? "};
     let response2 = {"text" : "Cheik htamein/Hpi skirt/Simple htamein.",
