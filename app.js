a9045de81fea604eafb2d0d5c8371d72f3f6db33
sandarmyint <1sandarmyint1@gmail.com>
2020-04-24 20:59:49 +0630

    renew

--- app.js
+++ app.js
@@ -57,7 +57,6 @@ let userInfo = {
   price : false,
 };
 
-
 let designAttachment = false;
 let bdesignAttachment = false;
 let sharepicAttachment = false;
@@ -65,6 +64,12 @@ let sharepicAttachment = false;
 let userEnteredInfo = {};
 let userSendAttachment = {};
 
+let payload = {
+  recipient: {
+    id : sender_psid
+  },
+  sender_action : 'trying_on'
+}
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
