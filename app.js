3b428c1580a4b152997394df3b3bba489eadd871
sandarmyint <1sandarmyint1@gmail.com>
2020-04-25 16:46:47 +0630

    renew

--- app.js
+++ app.js
@@ -172,6 +172,16 @@ function handleMessage(sender_psid, received_message) {
   /*****************************************/
   else if (received_message.text == "done" || received_message.text == "Done" ) {
     showAllDataToCus (sender_psid);
+  }else if (received_message.quick_reply.payload  == "change_sure") {    
+    let response1 = {"text": "Ok... is there anything you want to change then type the key word that you want to change. :) "};
+    let response2 = {"text" : " If there is nothing to change write 'Done' to view update record. :)"}
+    callSend(sender_psid, response1).then(()=>{
+      return callSend(sender_psid, response2);
+    });
+  }else if (received_message.quick_reply.payload  === "change_chestno") {
+   response = { "text" : "Send me update measurement. :)"}
+   changing.chest = true;
+   changing.upperArm = true;
   }else if (received_message.text == "Chest" || received_message.text == "chest" ) {
    response = { "text" : "Send me update measurement. :)"}
    changing.chest = true;
@@ -191,15 +201,25 @@ function handleMessage(sender_psid, received_message) {
                         }]
     }
     changing.chest = false;
-  }else if (received_message.quick_reply.payload  === "change_chestno") {
-   response = { "text" : "Send me update measurement. :)"}
-   changing.chest = true;
-  }else if (received_message.quick_reply.payload  == "change_sure") {    
-    let response1 = {"text": "Ok... is there anything you want to change then type the key word that you want to change. :) "};
-    let response2 = {"text" : " If there is nothing to change write 'Done' to view update record. :)"}
-    callSend(sender_psid, response1).then(()=>{
-      return callSend(sender_psid, response2);
-    });
+  }else if (received_message.text == "Arm" || received_message.text == "arm" ) {
+   response = { "text" : "Send me update measurement. :) :)"}
+   changing.upperArm = true;
+  }else if (received_message.text && changing.upperArm == true) {   
+    userEnteredInfo.upperArm =  received_message.text;
+    response = {
+      "text": `Are you sure?`,
+      "quick_replies":[
+                        {
+                        "content_type":"text",
+                        "title":"Sure",
+                        "payload":"change_sure"
+                        },{
+                        "content_type":"text",
+                        "title":"No",
+                        "payload":"change_chestno"
+                        }]
+    }
+    changing.upperArm = false;
   }
   /****************************************************************/
   else if (received_message.text == "change" || received_message.text == "Change") {
