54a0f599aa4325a66408881a241aa4a3fd86030c
sandarmyint <1sandarmyint1@gmail.com>
2020-04-25 16:12:12 +0630

    renew

--- app.js
+++ app.js
@@ -57,6 +57,20 @@ let userInfo = {
   price : false,
 };
 
+let changing = {
+  chest:false,
+  upperArm:false,
+  sleevelength:false,
+  waist:false,
+  hips:false,
+  thigh:false,
+  inseam:false,
+  htameintype:false,
+  htameinfold:false,
+  khar:false,
+  ankle:false,
+};
+
 
 let designAttachment = false;
 let bdesignAttachment = false;
@@ -155,31 +169,31 @@ function handleMessage(sender_psid, received_message) {
    saveData (sender_psid);
    response = { "text" : "OK :)"}
   }
-  // else if (received_message.text == "Chest" || received_message.text == "chest") {
-  //  response = { "text" : "Send me update measurement. :)"}
-  //  userInfo.chest = true;
-  // }else if (received_message.text && userInfo.chest == true) {   
-  //   userEnteredInfo.chest =  received_message.text;
-  //   response = {
-  //     "text": `Are you sure?`,
-  //     "quick_replies":[
-  //                       {
-  //                       "content_type":"text",
-  //                       "title":"Sure",
-  //                       "payload":"change_sure"
-  //                       },{
-  //                       "content_type":"text",
-  //                       "title":"No",
-  //                       "payload":"change_chestno"
-  //                       }]
-  //   }
-  //   userInfo.chest = false;
-  // }else if (received_message.payload === "change_chestno") {
-  //  response = { "text" : "Send me update measurement. :)"}
-  //  userInfo.chest = true;
-  // }else if (received_message.payload == "change_sure") {    
-  //   response = {"text": "Ok... is there anything you want to change then type the key word that you want to change.:) "}
-  // }
+  else if (received_message.text == "Chest" || received_message.text == "chest" && changing.chest == true) {
+   response = { "text" : "Send me update measurement. :)"}
+   userInfo.chest = true;
+  }else if (received_message.text && userInfo.chest == true) {   
+    userEnteredInfo.chest =  received_message.text;
+    response = {
+      "text": `Are you sure?`,
+      "quick_replies":[
+                        {
+                        "content_type":"text",
+                        "title":"Sure",
+                        "payload":"change_sure"
+                        },{
+                        "content_type":"text",
+                        "title":"No",
+                        "payload":"change_chestno"
+                        }]
+    }
+    userInfo.chest = false;
+  }else if (received_message.payload === "change_chestno") {
+   response = { "text" : "Send me update measurement. :)"}
+   userInfo.chest = true;
+  }else if (received_message.payload == "change_sure") {    
+    response = {"text": "Ok... is there anything you want to change then type the key word that you want to change.:) "}
+  }
   else if (received_message.text == "change" || received_message.text == "Change") {
    response = {
     "attachment":{
@@ -439,6 +453,7 @@ function handlePostback(sender_psid, received_postback) {
     callSend(sender_psid, response1).then(()=>{
       return callSend(sender_psid, response2);
     });
+    changing.chest = true;
   }else if (payload === 'change_htamein') {
     let response1 = { "text" : "Type what you want to change. Please write the same as key word which you want to change. The key words are"};
     let response2 = { "text" : "Type/ Fold/ Khar/ Ankle."};
