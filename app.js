60d94e87eaadc42b7bed638408af09936f4698b5
sandarmyint <1sandarmyint1@gmail.com>
2020-04-27 14:17:19 +0630

    renew

--- app.js
+++ app.js
@@ -172,6 +172,19 @@ handlePostback(sender_psid, webhook_event.postback);
 
 });
 
+function handleQuickReply (sender_psid, received_message) {
+  console.log('ok')
+   let response;
+  let payload = received_message.quick_reply.payload;
+  if (received_message.quick_reply.payload  == "change_sure") {    
+    let response1 = {"text": "Ok... is there anything you want to change then type the key word that you want to change. :) "};
+    let response2 = {"text" : " If there is nothing to change write 'Done' to view update record. :)"}
+    callSend(sender_psid, response1).then(()=>{
+      return callSend(sender_psid, response2);
+    });
+  }
+}
+
 function handleMessage(sender_psid, received_message) {
   let response;
   if (received_message.text == "hi" || received_message.text == "Hi") {    
