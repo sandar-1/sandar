af223f879fa336a3c61c88dd9d16b63f8e9907e7
sandarmyint <1sandarmyint1@gmail.com>
2020-04-27 18:59:44 +0630

    renew

--- app.js
+++ app.js
@@ -631,7 +631,9 @@ function handlePostback(sender_psid, received_postback) {
   console.log('ok')
    let response;
   let payload = received_postback.payload;
-  if (payload === 'yes_right') {
+  if (payload === 'get_started') {
+    greetUser (sender_psid);
+  }else if (payload === 'yes_right') {
     askforevent (sender_psid);
   }else if (payload === 'WEDDING') {
     wedding_event (sender_psid);
