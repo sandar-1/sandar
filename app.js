3408fb02b76069148cb0ac84e90167980f28452d
sandarmyint <1sandarmyint1@gmail.com>
2020-04-27 15:54:10 +0630

    renew

--- app.js
+++ app.js
@@ -414,6 +414,31 @@ function handleMessage(sender_psid, received_message) {
                         }]
     }
     changing.chest = false;
+  }else if (received_message.text == "Arm" || received_message.text == "arm" ) {
+   response = { "text" : "Send me update measurement. :)"}
+   changing.upperArm = true;
+  }else if (received_message.text && changing.upperArm == true) {   
+    userEnteredInfo.upperArm =  received_message.text;
+    response = {
+      "text": `Are you sure?`,
+      "quick_replies":[
+                        {
+                        "content_type":"text",
+                        "title":"Sure",
+                        "payload":"change_sure"
+                        },{
+                        "content_type":"text",
+                        "title":"No",
+                        "payload":"change_armno"
+                        }]
+    }
+    changing.upperArm = false;
+  }else if (received_message.text == "Sure") {    
+    let response1 = {"text": "Ok... is there anything you want to change then type the key word that you want to change. :) "};
+    let response2 = {"text" : " If there is nothing to change write 'Done' to view update record. :)"}
+    callSend(sender_psid, response1).then(()=>{
+      return callSend(sender_psid, response2);
+    });
   }
   callSendAPI(sender_psid, response);    
 }
