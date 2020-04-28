b74ce5ecd38f7b077c71824d56339f41197d41bb
sandarmyint <1sandarmyint1@gmail.com>
2020-04-28 13:03:40 +0630

    renew

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ const
   express = require('express'),
   body_parser = require('body-parser'),
   firebase = require("firebase-admin"),
-   ejs = require("ejs"),
   app = express().use(body_parser.json()); // creates express http server
 
   firebase.initializeApp({
