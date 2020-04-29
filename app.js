90541807daff5ea1dc284c5db2149aeb537426af
sandarmyint <1sandarmyint1@gmail.com>
2020-04-29 18:28:29 +0630

    renew

--- app.js
+++ app.js
@@ -42,32 +42,28 @@ const
 let db = firebase.firestore();
 
 // Accepts POST requests at /webhook endpoint
-app.post('/webhook', (req, res) => {  
+app.post('/webhook', (req, res) => {
 
   // Parse the request body from the POST
   let body = req.body;
-
+  
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
+    body.entry.forEach(function (entry) {
 
-    body.entry.forEach(function(entry) {
-
-      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-
-      // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
-      console.log('Sender ID: ' + sender_psid);   
 
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);        
+        if (webhook_event.message.quick_reply) {
+          handleQuickReply(sender_psid, webhook_event.message.quick_reply.payload);
+        } else {
+          handleMessage(sender_psid, webhook_event.message);
+        }
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback);
       }
-      
+
     });
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
