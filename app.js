9f56f7fe2bb859435627e376cc9ae0eae6bce29a
sandarmyint <1sandarmyint1@gmail.com>
2020-04-29 18:17:50 +0630

    renew

--- app.js
+++ app.js
@@ -101,28 +101,31 @@ app.get('/whitelists', function (req, res) {
 
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
-  
+
   /** UPDATE YOUR VERIFY TOKEN **/
   const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-  
+
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
 
+
+
   // Check if a token and mode were sent
   if (mode && token) {
-    
+
+
     // Check the mode and token sent are correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+
       // Respond with 200 OK and challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-    
+
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 });
@@ -172,7 +175,6 @@ const handlePostback = (sender_psid, received_postback) => {
     default:
       defaultReply(sender_psid);
   }
-  callSendAPI(sender_psid, response);
 }
 
 /*function to greet user*/
