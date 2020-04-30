414781086ce75da5bcab6d35c5761cbb2b195f82
sandarmyint <1sandarmyint1@gmail.com>
2020-04-30 13:21:29 +0630

    renew

--- app.js
+++ app.js
@@ -55,11 +55,7 @@ app.post('/webhook', (req, res) => {
       let sender_psid = webhook_event.sender.id;
 
       if (webhook_event.message) {
-        if (webhook_event.message.quick_reply) {
-          handleQuickReply(sender_psid, webhook_event.message.quick_reply.payload);
-        } else {
-          handleMessage(sender_psid, webhook_event.message);
-        }
+        handleMessage(sender_psid, webhook_event.message);        
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback);
       }
