646112f19116030117c68dc05c59a57d33f62a02
sandarmyint <1sandarmyint1@gmail.com>
2020-05-09 21:07:39 +0630

    renew

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ app.post('/imagepick',function(req,res){
     if (!doc.exists) {
       console.log('No such document!');
     } else {
-      const image_url = doc.data().url;
+      const image_url = doc.data().weddingPic;
 
       console.log('IMG URL:', image_url);
 
