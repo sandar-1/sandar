a9ef7038e95b8747b56df1e6fea6aeabe76274a7
sandarmyint <1sandarmyint1@gmail.com>
2020-05-11 00:36:38 +0630

    re

--- app.js
+++ app.js
@@ -862,12 +862,12 @@ function handleMessage(sender_psid, received_message) {
     response = {"text" : "Please send me again"}
     paidAttachment == true;
   }else if (received_message.text == "Yes" && bothSave == true) {    
-   // saveData_both(sender_psid);
+    saveData_both(sender_psid);
     response = {"attachment":{
                       "type":"template",
                       "payload":{
                         "template_type":"button",
-                        "text":"Thank you. :)",
+                        "text":"💗 🤗.Thank you.🤗 💗",
                         "buttons":[
                           {
                             "type":"web_url",
@@ -881,12 +881,12 @@ function handleMessage(sender_psid, received_message) {
     paidAttachment = false;
     bothSave = false;
   }else if (received_message.text == "Yes" && ypSave == true) {    
-   // saveData_YP(sender_psid);
+   saveData_YP(sender_psid);
     response = {"attachment":{
                       "type":"template",
                       "payload":{
                         "template_type":"button",
-                        "text":"Thank you. :) :)",
+                        "text":"💗 🤗.Thank you.🤗 💗",
                         "buttons":[
                           {
                             "type":"web_url",
@@ -900,12 +900,12 @@ function handleMessage(sender_psid, received_message) {
     paidAttachment = false;
     ypSave = false
   }else if (received_message.text == "Yes" && hmSave == true) {    
-   // saveData_HM(sender_psid);
+   saveData_HM(sender_psid);
     response = {"attachment":{
                       "type":"template",
                       "payload":{
                         "template_type":"button",
-                        "text":"Thank you. :)",
+                        "text":"💗 🤗.Thank you.🤗 💗",
                         "buttons":[
                           {
                             "type":"web_url",
